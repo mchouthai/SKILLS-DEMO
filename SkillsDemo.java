586759bebd6aca71bf6485e57b4669faca53834c
mchouthai <64107869+mchouthai@users.noreply.github.com>
2022-05-01 15:27:02 -0700

    files added

--- SkillsDemo.java
+++ SkillsDemo.java
@@ -1,6 +1,9 @@
 public class SkillsDemo{
 
 //insert code
+    public static double divide(int n, int d){
+        return (double) n / d;
+    }
 
 
 
---
+++ SkillsDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillsDemoTest {
+
+    @Test
+    public void division() {
+        assertEquals(2.5, SkillsDemo.divide(5,3), 0.001);
+}
+    
+}
